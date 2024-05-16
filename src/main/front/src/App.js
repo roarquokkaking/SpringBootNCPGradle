a7d6bfda0b1685bbc651ab6ace4cd03ed957bdd4
roarquokkaking <ldw9581@naver.com>
2024-05-16 12:51:53 +0900

    1

---
+++ src/main/front/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h3>Spring Boot  + React + NCP zz z</h3>
+      <img src='/logo512.png' alt='기본이미지'/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/test/java/com/example/SpringBootNCPGradle/SpringBootNcpGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootNCPGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNcpGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
